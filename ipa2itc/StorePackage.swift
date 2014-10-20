4107a2917b583e94fa66a810fd47f2d898bceb96
Emlyn Murphy <me@emlyn.net>
2014-10-20 19:32:00 -0400

    Adding properties to the StorePackage class.

--- ipa2itc/StorePackage.swift
+++ ipa2itc/StorePackage.swift
@@ -8,6 +8,13 @@
 
 import Cocoa
 
-class StorePackage {
-
+public class StorePackage {
+    public let bundleVersion: String?
+    public let shortBundleVersion: String?
+    public let bundleIdentifier: String?
+    public let fileURL: NSURL
+    
+    public init(fileURL: NSURL) {
+        self.fileURL = fileURL
+    }
 }
