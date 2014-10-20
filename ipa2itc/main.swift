0b29667f48f19c0071335e2901c50e3c591a1f9e
Emlyn Murphy <me@emlyn.net>
2014-10-20 18:27:00 -0400

    Created an OS X Xcode project.

---
+++ ipa2itc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ipa2itc
+//
+//  Created by Emlyn Murphy on 10/20/14.
+//  Copyright (c) 2014 Nitemotif. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
